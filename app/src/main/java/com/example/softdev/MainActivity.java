b29b397b7e0c2f92fc070ace717ceefc7e994bb1
Thirdeyee1 <carlostbautista@su.edu.ph>
2023-10-17 17:04:26 +0800

    V.1.1 commit

---
+++ app/src/main/java/com/example/softdev/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.softdev;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button add;
+    AlertDialog dialog;
+    LinearLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        add = findViewById(R.id.add);
+        layout = findViewById(R.id.container);
+
+        buildDialog();
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+            }
+        });
+    }
+
+    private void buildDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog, null);
+
+        final EditText name = view.findViewById(R.id.nameEdit);
+        final EditText load = view.findViewById(R.id.loadEdit);
+        final EditText quantity = view.findViewById(R.id.quantityEdit);
+        final EditText hrsDaily = view.findViewById(R.id.hrsDailyEdit);
+        final EditText days = view.findViewById(R.id.daysEdit);
+
+        builder.setView(view);
+        builder.setTitle("Enter Details")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String nameStr = name.getText().toString();
+                        String loadStr = load.getText().toString();
+                        String quantityStr = quantity.getText().toString();
+                        String hrsDailyStr = hrsDaily.getText().toString();
+                        String daysStr = days.getText().toString();
+
+                        addCard(nameStr, loadStr, quantityStr, hrsDailyStr, daysStr);
+
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        dialog = builder.create();
+    }
+
+    private void addCard(String name, String load, String quantity, String hrsDaily, String days) {
+        final View view = getLayoutInflater().inflate(R.layout.card, null);
+
+        TextView nameView = view.findViewById(R.id.name);
+        TextView loadView = view.findViewById(R.id.load);
+        TextView quantityView = view.findViewById(R.id.quantity);
+        TextView hrsDailyView = view.findViewById(R.id.hrsDaily);
+        TextView daysDailyView = view.findViewById(R.id.days);
+
+        Button delete = view.findViewById(R.id.delete);
+
+        nameView.setText(name);
+        loadView.setText(load);
+        quantityView.setText(quantity);
+        hrsDailyView.setText(hrsDaily);
+        daysDailyView.setText(days);
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.removeView(view);
+            }
+        });
+
+        layout.addView(view);
+    }
+}
