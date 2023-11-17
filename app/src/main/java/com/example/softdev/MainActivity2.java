b4d1588a758f1e2aaee7a084ca9ff50ff1b9682c
Thirdeyee1 <carlostbautista@su.edu.ph>
2023-11-18 04:38:19 +0800

    Added second window Added Go back button in second window Added Text views Fixed minor bugs Fixed crashing Added spacing in card Added Example slot Programed example slot to terminate when new slow s added

---
+++ app/src/main/java/com/example/softdev/MainActivity2.java
@@ -0,0 +1,33 @@
+package com.example.softdev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    Button back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        back = (Button) findViewById(R.id.btnGoBack);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+
+            }
+        });
+
+
+    }
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
+    }
+}
