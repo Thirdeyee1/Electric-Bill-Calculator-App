52c8eb5c9bbaa25b6a1f296381b4d994c9232190
CrimsonSax <agapito.paolo7@gmail.com>
2023-11-20 19:26:49 +0800

    Added Total Calculation  w/ issue and Code Changes
    Total Calculation:
    So ni gamit nlng kog default linked list nga double ray i dawat (ang computedDaily) para ma update ang total everytime mu add og appliace.
    
    Ang known issue sa pag delete sa card ug pag update sa total kay dapat in order para ma back to 0 ang total. di pako sure unsaon kanang ang kadtong specific card ray ma deduct. Kung dili i by order ang delete kay ang value sa previous card ang ma deduct sa total, dili ang any cards. Hopefully ma pangitaan rag paagi nga kada make sa card kay mu generate og unique identifier para ig press sa iyang own delete button kay ma deduct ra accordingly.
    
    Code Changes:
    gi accept ra nako tung suggestion sa android studio nga gamiton og lambda intstead adtung  "@Override public void onClick" i vew ra ang changes

--- app/src/main/java/com/example/softdev/MainActivity.java
+++ app/src/main/java/com/example/softdev/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.softdev;
 
 import static android.text.TextUtils.*;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,15 +14,90 @@ import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+class Node {
+    double data;
+    Node next;
+
+    Node(double data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    private Node head;
+
+    LinkedList() {
+        this.head = null;
+    }
+
+    // Method to add a new node with given data at the end of the list
+    public void add(double data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    // Method to delete a node at a specified index
+    public void deleteNodeAt(int index) {
+        if (head == null) {
+            return;
+        }
+
+        if (index == 0) {
+            head = head.next;
+            return;
+        }
+
+        Node temp = head;
+        Node prev = null;
+        int count = 0;
+
+        while (temp != null) {
+            if (count == index) {
+                prev.next = temp.next;
+                return;
+            }
+            prev = temp;
+            temp = temp.next;
+            count++;
+        }
+
+    }
+
+    // Method to calculate the sum of all values in the list
+    public double sumOfValues() {
+        Node temp = head;
+        double sum = 0.0;
+
+        while (temp != null) {
+            sum += temp.data;
+            temp = temp.next;
+        }
+
+        return sum;
+    }
+}
+
 public class MainActivity extends AppCompatActivity {
+    LinkedList myList = new LinkedList();
 
     Button add;
     AlertDialog dialog;
     LinearLayout layout;
 
-    private View exampleSlotView;
-    boolean initialSlotReplaced = false;
 
+    private View exampleSlotView;
+    private int cardIndex = 0;
+    boolean initialSlotReplaced = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +107,7 @@ public class MainActivity extends AppCompatActivity {
 
         //For the new window
         Button summary = findViewById(R.id.summary);
-        summary.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity2();
-            }
-        });
+        summary.setOnClickListener(v -> openActivity2());
 
         //For building dialog
         add = findViewById(R.id.add);
@@ -53,17 +122,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                initialSlotReplaced = true;
-                dialog.show();
-            }
+        add.setOnClickListener(v -> {
+            initialSlotReplaced = true;
+            dialog.show();
         });
     }
     public void openActivity2(){
         Intent intent = new Intent(this, MainActivity2.class);
         startActivity(intent);
+    }
+
+    public void writeTotal(Double t_d){
+        TextView viewT_D = findViewById(R.id.total_daily);
+        TextView viewT_M = findViewById(R.id.total_monthly);
+
+        myList.add(t_d);
+        double sum = myList.sumOfValues();
+
+        viewT_D.setText(String.format("%.2f",sum));
+        viewT_M.setText(String.format("%.2f",sum*31));
+    }
+
+    public void writeTotal(){
+        TextView viewT_D = findViewById(R.id.total_daily);
+        TextView viewT_M = findViewById(R.id.total_monthly);
+
+        double sum = myList.sumOfValues();
+
+        viewT_D.setText(String.format("%.2f",sum));
+        viewT_M.setText(String.format("%.2f",sum*31));
     }
     private void buildDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -76,46 +163,39 @@ public class MainActivity extends AppCompatActivity {
 
         builder.setView(view);
         builder.setTitle("Enter Details");
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String nameStr = name.getText().toString();
-                String loadStr = load.getText().toString();
-                String quantityStr = quantity.getText().toString();
-                String hrsDailyStr = hrsDaily.getText().toString();
+        builder.setPositiveButton("OK", (dialog, which) -> {
+            String nameStr = name.getText().toString();
+            String loadStr = load.getText().toString();
+            String quantityStr = quantity.getText().toString();
+            String hrsDailyStr = hrsDaily.getText().toString();
 
-                if (isEmpty(nameStr) || isEmpty(loadStr) || isEmpty(quantityStr) || isEmpty(hrsDailyStr)) {
-                    Toast.makeText(MainActivity.this, "Please input details", Toast.LENGTH_SHORT).show();}
-                else if (isEmpty(nameStr)) {
-                    Toast.makeText(MainActivity.this, "Please input Device name", Toast.LENGTH_SHORT).show();
-                } else if (isEmpty(loadStr) || isEmpty(quantityStr) || isEmpty(hrsDailyStr)){
+            if (isEmpty(nameStr) || isEmpty(loadStr) || isEmpty(quantityStr) || isEmpty(hrsDailyStr)) {
+                Toast.makeText(MainActivity.this, "Please input details", Toast.LENGTH_SHORT).show();}
+            else if (isEmpty(nameStr)) {
+                Toast.makeText(MainActivity.this, "Please input Device name", Toast.LENGTH_SHORT).show();
+            } else if (isEmpty(loadStr) || isEmpty(quantityStr) || isEmpty(hrsDailyStr)){
+                Toast.makeText(MainActivity.this, "Please input valid values", Toast.LENGTH_SHORT).show();
+            } else {
+                try {
+                    // Attempt to convert input to numbers
+                    Double.parseDouble(loadStr);
+                    Double.parseDouble(quantityStr);
+                    Double.parseDouble(hrsDailyStr);
+
+                    // Adding card if conversion is successful
+                    addCard(nameStr, loadStr, quantityStr, hrsDailyStr);
+                } catch (NumberFormatException e) {
+                    // Show a toast if conversion to numbers fails
                     Toast.makeText(MainActivity.this, "Please input valid values", Toast.LENGTH_SHORT).show();
-                } else {
-                    try {
-                        // Attempt to convert input to numbers
-                        Double.parseDouble(loadStr);
-                        Double.parseDouble(quantityStr);
-                        Double.parseDouble(hrsDailyStr);
-
-                        // Adding card if conversion is successful
-                        addCard(nameStr, loadStr, quantityStr, hrsDailyStr);
-                    } catch (NumberFormatException e) {
-                        // Show a toast if conversion to numbers fails
-                        Toast.makeText(MainActivity.this, "Please input valid values", Toast.LENGTH_SHORT).show();
-                    }
                 }
             }
         });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
+        builder.setNegativeButton("Cancel", (dialog, which) -> {
 
-            }
         });
 
         dialog = builder.create();
     }
-
     private void addCard(String name, String load, String quantity, String hrsDaily) {
         removeExampleSlot();
 
@@ -128,8 +208,10 @@ public class MainActivity extends AppCompatActivity {
 
         TextView viewD = view.findViewById(R.id.costDaily);
         TextView viewM = view.findViewById(R.id.costMonthly);
-
         Button delete = view.findViewById(R.id.delete);
+
+        int currentIndex = cardIndex;
+        ++cardIndex;
 
         nameView.setText(name);
         loadView.setText(load);
@@ -139,22 +221,25 @@ public class MainActivity extends AppCompatActivity {
         double l = Double.parseDouble(String.valueOf(load));
         double q = Double.parseDouble(String.valueOf(quantity));
         double h = Double.parseDouble(String.valueOf(hrsDaily));
+        double computedDaily = 10 * l / 1000 * q * h;
+        double computedMonthly = computedDaily * 31;
 
+        writeTotal(computedDaily);
 
+        viewD.setText(String.format("%.2f",computedDaily));
+        viewM.setText(String.format("%.2f",computedMonthly));
 
-        viewD.setText(String.format("%.2f", 11.85*l/1000*q*h));
-        viewM.setText(String.format("%.2f", 11.85*l/1000*q*h*31));
-
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                layout.removeView(view);
-            }
+        delete.setOnClickListener(v -> {
+            layout.removeView(view);
+            myList.deleteNodeAt(currentIndex); // Pass the index of the deleted card
+            --cardIndex;
+            writeTotal();
         });
 
         layout.addView(view);
     }
 
+    // EXAMPLES BELOW
     //DELETE EXAMPLE SLOT
     private void removeExampleSlot() {
         // Remove the example slot from the layout
@@ -184,9 +269,12 @@ public class MainActivity extends AppCompatActivity {
         double l = Double.parseDouble(String.valueOf(load));
         double q = Double.parseDouble(String.valueOf(quantity));
         double h = Double.parseDouble(String.valueOf(hrsDaily));
+        double computedDaily = 11.85 * l / 1000 * q * h;
+        double computedMonthly = computedDaily * 31;
 
-        viewD.setText(String.format("%.2f", 11.85 * l / 1000 * q * h));
-        viewM.setText(String.format("%.2f", 11.85 * l / 1000 * q * h * 31));
+
+        viewD.setText(String.format("%.2f",computedDaily));
+        viewM.setText(String.format("%.2f",computedMonthly));
 
         layout.addView(exampleSlotView);
         return exampleSlotView;
