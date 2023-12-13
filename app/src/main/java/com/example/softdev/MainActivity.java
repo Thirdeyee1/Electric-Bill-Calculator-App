eaaf42e07b60ea89ac6abec3fa27b2c4742d19e1
Eldonex <151842097+Eldonex@users.noreply.github.com>
2023-12-13 23:32:12 +0800

    Tip box done

--- app/src/main/java/com/example/softdev/MainActivity.java
+++ app/src/main/java/com/example/softdev/MainActivity.java
@@ -3,15 +3,21 @@ package com.example.softdev;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout layout;
-
+    HorizontalScrollView scrollView;
+    int currentCardIndex = 0;
+    final int totalCards = 4;
+    final int scrollDelay = 3000; // 3 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +31,42 @@ public class MainActivity extends AppCompatActivity {
         goAutomatic.setOnClickListener(v -> openAutomatic());
 
         layout = findViewById(R.id.tip_box);
-        for (int i = 0; i < 4; i++) {
-            addCard();
+        scrollView = findViewById(R.id.horizontal_scroll_view);
+
+        // Add cards with different tips
+        for (int i = 1; i <= totalCards; i++) {
+            addCard(i);
         }
 
+        // Start scrolling handler
+        startCardScrolling();
     }
 
+    private void startCardScrolling() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                scrollNextCard();
+                startCardScrolling();
+            }
+        }, scrollDelay);
+    }
 
-    @SuppressLint("DefaultLocale") private void addCard() {
+    private void scrollNextCard() {
+        currentCardIndex = (currentCardIndex + 1) % totalCards;
+        int scrollX = currentCardIndex * scrollView.getWidth();
+        scrollView.smoothScrollTo(scrollX, 0);
+    }
+
+    @SuppressLint("DefaultLocale")
+    private void addCard(int tipNumber) {
         @SuppressLint("InflateParams") final View view = getLayoutInflater().inflate(R.layout.card_tips, null);
-       // TextView tipView = view.findViewById(R.id.tip);
-       // tipView.setText();
+        TextView tipView = view.findViewById(R.id.tip_1); // Use the static ID
+        tipView.setText(getString(getResources().getIdentifier("tip_" + tipNumber, "string", getPackageName())));
         layout.addView(view);
     }
+
     public void openManual() {
         Intent intent = new Intent(this, ManualActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
